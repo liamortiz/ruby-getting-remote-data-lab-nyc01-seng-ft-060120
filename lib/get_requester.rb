33d75cc2fdf3c1bf8f19d13ae9195774b22add04
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-13 16:21:06 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,5 +3,15 @@ class GetRequester
   
   def initialize(url)
   end
+    
+  def get_response_body
+    uri = URI.parse(self.URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  
+  def parse_json
+    JSON.parse(self.get_response_body)
+  end
 
 end
